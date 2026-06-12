268cd7a738678a80c5899af550190b2c9ccb13c7
Tiago Matias <tiagomatias072@gmail.com>
2026-06-12 07:12:39 -0400

    chore: update and synchronize local node_modules dependencies

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,16 +2,4 @@ import { defineConfig } from "nitro";
 
 export default defineConfig({
   preset: "vercel",
-  publicAssets: [
-    {
-      dir: "./dist/client",
-      baseURL: "/",
-    },
-  ],
-  handlers: [
-    {
-      route: "/**",
-      handler: "./src/server.ts",
-    },
-  ],
 });
