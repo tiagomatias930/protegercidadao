e0b11f6596c8286c8901873bc659012716c6923f
Tiago Matias <tiagomatias072@gmail.com>
2026-06-12 07:08:34 -0400

    chore: configure nitro presets and update build script to include nitro build

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,4 +2,16 @@ import { defineConfig } from "nitro";
 
 export default defineConfig({
   preset: "vercel",
+  publicAssets: [
+    {
+      dir: "./dist/client",
+      baseURL: "/",
+    },
+  ],
+  handlers: [
+    {
+      route: "/**",
+      handler: "./src/server.ts",
+    },
+  ],
 });
